a8bae02f19f60fce222fae6f117eb1c5d4906336
Philipp Oleynik <pholey@utu.fi>
2020-02-27 20:51:26 +0200

    main state machine

--- lab_5/lab_5.srcs/sources_1/new/main_FSM.vhd
+++ lab_5/lab_5.srcs/sources_1/new/main_FSM.vhd
@@ -21,6 +21,11 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+library work;
+use work.types.all;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -34,22 +39,19 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity main_FSM is
     Port ( clock : in STD_LOGIC;
            reset : in STD_LOGIC;
-           color : out STD_LOGIC;
+           color : out Color_Type;
            direction : in STD_LOGIC);
 end main_FSM;
 
 architecture Behavioral of main_FSM is
-    type Color_Type         is (Red, Green, Blue, White, Black, Yellow, Purple);
-    type Color_State_Type   is (Red, Green, Blue, Alarm, Standby, Yellow, Purple);
-    
+   
     signal Color_Drive      : Color_Type; 
-
-    variable Color_State      : Color_State_Type; 
-
+  
     begin
+main_state_machine: process
+    variable tickcounter: integer range 0 to 7;
+    variable Color_State  : Color_State_Type; 
 
-    main_state_machine: process
-    variable tickcounter: integer range 0 to 15;
     begin
     if reset='1' then
         Color_State     := Standby;
@@ -61,74 +63,62 @@ architecture Behavioral of main_FSM is
             WHEN Red =>
                 Color_Drive<=Red;
                 if direction='1' then
-                    Color_State<= Yellow;
+                    Color_State:= Yellow;
                 else
-                    Color_State<= Yellow;
+                    Color_State:= Yellow;
                 end if;
                 
             WHEN Green =>
                 Color_Drive<=Green;   
                 if direction='1' then
-                Color_State<= Blue;
+                Color_State:= Blue;
             else
-                Color_State<= Yellow;
+                Color_State:= Yellow;
             end if;
             
             WHEN Blue => 
                 Color_Drive<=Blue;
                 if direction='1' then
-                Color_State<= Purple;
+                Color_State:= Purple;
             else
-                Color_State<= Green;
+                Color_State:= Green;
             end if;
             
-            WHEN Alarm => 
-                Color_Drive<=White;
-                tickcounter:=tickcounter+1;
-                if tickcounter = 7 then
-                    tickcounter:= 0;
-                    Color_State<= Standby;
-                end if;
-                        
             WHEN Standby =>
                 Color_Drive<=White;
                 tickcounter:=tickcounter+1;
                 if tickcounter = 4 then
                     tickcounter:= 0;
                     if direction='1' then
-                        Color_State<= Red;
+                        Color_State:= Red;
                     else
-                        Color_State<= Purple;
+                        Color_State:= Purple;
                     end if;
                 end if;
             
             WHEN Yellow => 
                 Color_Drive<=Yellow;
                 if direction='1' then
-                Color_State<= Green;
+                Color_State:= Green;
             else
-                Color_State<= Red;
+                Color_State:= Red;
             end if;
-            
             
             WHEN Purple => 
                 Color_Drive<=Purple;
                 if direction='1' then
-                Color_State<= Red;
+                Color_State:= Red;
             else
-                Color_State<= Blue;
+                Color_State:= Blue;
             end if;
             
             WHEN others =>      -- just in case
                 Color_Drive <= White; 
-                Color_State <= Standby;
+                Color_State := Standby;
                 tickcounter:= 2;
 
         end case;
     end if;
     wait on clock, reset;
     end process;
-
-
-
 end Behavioral;
---
+++ lab_5/lab_5.srcs/sources_1/new/types.vhd
@@ -0,0 +1,14 @@
+package types is
+ 
+  constant MASTERFREQ : integer := 125000000;
+ 
+  type Color_Type         is (Red, Green, Blue, White, Black, Yellow, Purple);
+  type Color_State_Type   is (Red, Green, Blue, Alarm, Standby, Yellow, Purple);
+
+end package types;
+
+package body types is
+
+--  Nothing here
+ 
+end package body types;
